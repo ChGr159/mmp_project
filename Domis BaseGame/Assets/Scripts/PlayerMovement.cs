e9d5727429700508c8afb45f2f5cc1c3bb02e59b
MjSilvas <Dominic-Lenz@web.de>
2021-06-16 18:10:03 +0200

    Domis BaseGame hinzugefügt

--- Domis BaseGame/Assets/Scripts/PlayerMovement.cs
+++ Domis BaseGame/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public PlayerController controller;             //using our CharakterScript
     public Animator animator;
     float horizontalMove = 0f;
+    float climbing = 0f;
     public float runSpeed = 40f;
     bool jump = false;
     bool crouch = false;
@@ -19,11 +20,15 @@ public class PlayerMovement : MonoBehaviour
 
         animator.SetFloat("velocityX", Mathf.Abs(horizontalMove));
 
+
+        // Lässt unserern Charakter springen
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
             animator.SetBool("Jump", true);
         }
+
+        // Lässt unseren Charakter sich hinknien
         if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
@@ -31,6 +36,8 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetButtonUp("Crouch"))
             crouch = false;
     }
+
+    // Funktion die der Variablen "Jump" ein false gibt, wenn der Charakter landet
     public void OnLanding ()
     {
         animator.SetBool("Jump", false);
