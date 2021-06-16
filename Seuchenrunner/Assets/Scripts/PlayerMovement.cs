dbc00964cbc5faf28d2dbef853d53f014d22e1a4
Natalia Borysenko <borysenko@cip.ifi.lmu.de>
2021-06-16 19:47:29 +0200

    Import von Dominik

---
+++ Seuchenrunner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public PlayerController controller;             //using our CharakterScript
+    public Animator animator;
+    float horizontalMove = 0f;
+    float climbing = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("velocityX", Mathf.Abs(horizontalMove));
+
+
+        // Lässt unserern Charakter springen
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("Jump", true);
+        }
+
+        // Lässt unseren Charakter sich hinknien
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+            crouch = false;
+    }
+
+    // Funktion die der Variablen "Jump" ein false gibt, wenn der Charakter landet
+    public void OnLanding ()
+    {
+        animator.SetBool("Jump", false);
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+}
